216c7b6456cad0ba00a732ebdb8397236fda5b25
amirtanazzoh <92968872+amirtanazzoh@users.noreply.github.com>
2024-10-02 16:17:58 +0330

    1.3.3

--- create-tag.js
+++ create-tag.js
@@ -4,9 +4,28 @@ const { execSync } = require('child_process');
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN; // توکن را از محیط دریافت می‌کند
 const REPO_OWNER = 'cyandm'; // نام کاربری شما در گیت‌هاب
 const REPO_NAME = 'cyan-plugin'; // نام ریپازیتوری شما
-const TAG_NAME = process.env.GITHUB_REF.split('/').pop(); // استخراج نام تگ از GITHUB_REF
+const sha = process.env.GITHUB_SHA; // SHA آخرین کامیت
 
-async function createRelease() {
+async function getCommitMessage(repo, sha, token) {
+	const response = await fetch(
+		`https://api.github.com/repos/${repo}/commits/${sha}`,
+		{
+			headers: {
+				Authorization: `token ${token}`,
+				Accept: 'application/vnd.github.v3+json',
+			},
+		}
+	);
+
+	if (!response.ok) {
+		throw new Error('Failed to fetch commit message');
+	}
+
+	const data = await response.json();
+	return data.commit.message; // بازگشت پیام کامیت
+}
+
+async function createRelease(tagname) {
 	try {
 		// ایجاد تگ جدید
 		await axios.post(
@@ -27,8 +46,8 @@ async function createRelease() {
 		await axios.post(
 			`https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases`,
 			{
-				tag_name: TAG_NAME,
-				name: TAG_NAME,
+				tag_name: tagname,
+				name: tagname,
 				body: 'Release created automatically via GitHub Actions',
 			},
 			{
@@ -39,7 +58,7 @@ async function createRelease() {
 			}
 		);
 
-		console.log(`Release ${TAG_NAME} created successfully.`);
+		console.log(`Release ${tagname} created successfully.`);
 	} catch (error) {
 		console.error(
 			`Error: ${error.response ? error.response.data.message : error.message}`
@@ -47,5 +66,9 @@ async function createRelease() {
 	}
 }
 
-// اجرای تابع ایجاد ریلیز
-createRelease();
+(async () => {
+	const commitMessage = await getCommitMessage(repo, sha, token);
+	const TAG_NAME = commitMessage; // استفاده از پیام کامیت به عنوان نام تگ
+
+	createRelease(TAG_NAME);
+})();
